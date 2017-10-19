afd683933095662854414b4aa8d951860cee2146
Daniel Lennox <dlennox2424@gmail.com>
2017-10-19 16:14:32 -0600

    [2.1.0] Updating to ricro-app-template@1.0.6

--- src/index.js
+++ src/index.js
@@ -6,13 +6,8 @@ import App from 'ricro-app-template';
 import config from './config.json';
 import Apps from './Apps';
 
-const headerConfig = {
-  noLogin: true,
-  noHome: true,
-};
-
 ReactDOM.render(
-  <App config={config} headerConfig={headerConfig} reduxMiddleware={window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()}>
+  <App config={config} reduxMiddleware={window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()}>
     <Apps/>
   </App>,
   document.getElementById('root')
