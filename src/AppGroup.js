6cf81a48b255dc9336bdd938860381b32350cffa
Daniel Lennox <dlennox2424@gmail.com>
2018-05-31 16:43:25 -0600

    [4.0.0] Updating to colostate-ricro-ui@1.0.0

--- src/AppGroup.js
+++ src/AppGroup.js
@@ -1,90 +1,81 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {
-  withStyles,
-} from 'material-ui/styles';
-import Icon from 'material-ui/Icon';
-import List, {
-  ListItem,
-  ListItemText,
-  ListItemSecondaryAction,
-  ListItemIcon,
-} from 'material-ui/List';
-import IconButton from 'material-ui/IconButton';
-import Divider from 'material-ui/Divider';
-import Avatar from 'material-ui/Avatar';
-import Tooltip from 'material-ui/Tooltip';
-import Card, {
-  CardHeader,
-  CardContent
-} from 'material-ui/Card';
+import { withStyles } from '@material-ui/core/styles';
+import Icon from '@material-ui/core/Icon';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import IconButton from '@material-ui/core/IconButton';
+import Divider from '@material-ui/core/Divider';
+import Avatar from '@material-ui/core/Avatar';
+import Tooltip from '@material-ui/core/Tooltip';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
 
 const styles = theme => ({
   container: {
-    margin: theme.spacing.unit * 2 + 'px ' + theme.spacing.unit + 'px',
+    margin: `${theme.spacing.unit * 2}px ${theme.spacing.unit}px`,
   },
   appLink: {
     '&:hover': {
       textDecoration: 'none',
-    }
+    },
   },
   avatar: {
     backgroundColor: theme.palette.csuBrand.primary.green,
   },
 });
 
-const AppGroup = (props) => {
-  const {
-    appGroup,
-    classes,
-  } = props;
+const AppGroup = props => {
+  const { appGroup, classes } = props;
 
   return appGroup == null ? null : (
     <Card className={classes.container}>
-      <CardHeader title={appGroup.name} subheader={appGroup.shortName}/>
-      <Divider/>
+      <CardHeader title={appGroup.name} subheader={appGroup.shortName} />
+      <Divider />
       <CardContent>
         <List>
           {appGroup.apps.map((app, i) => {
-            return(
-              <div key={i}>
+            return (
+              <div key={app.name}>
                 <a href={app.link} className={classes.appLink}>
-                  <ListItem
-                    button
-                    >
+                  <ListItem button>
                     <Avatar className={classes.avatar}>
                       <Icon>{app.icon == null ? 'apps' : app.icon}</Icon>
                     </Avatar>
                     <ListItemText primary={app.name} secondary={app.description} />
                     {app.protected ? (
-                      <Tooltip title='Requires login'>
-                        <ListItemIcon aria-label='Requires login'>
+                      <Tooltip title="Requires login">
+                        <ListItemIcon aria-label="Requires login">
                           <Icon>lock</Icon>
                         </ListItemIcon>
                       </Tooltip>
                     ) : null}
                     <ListItemSecondaryAction>
-                      <Tooltip title='Open'>
-                        <IconButton aria-label='Open'>
+                      <Tooltip title="Open">
+                        <IconButton aria-label="Open">
                           <Icon>keyboard_arrow_right</Icon>
                         </IconButton>
                       </Tooltip>
                     </ListItemSecondaryAction>
                   </ListItem>
-                  {appGroup.apps.length-1 !== i && (<Divider/>)}
+                  {appGroup.apps.length - 1 !== i && <Divider />}
                 </a>
               </div>
-            )
+            );
           })}
         </List>
       </CardContent>
     </Card>
   );
-}
+};
 
 AppGroup.propTypes = {
-  classes: PropTypes.object.isRequired,
   appGroup: PropTypes.object,
+  classes: PropTypes.object.isRequired,
 };
 
 export default withStyles(styles)(AppGroup);
--- src/Apps.js
+++ src/Apps.js
@@ -1,52 +1,53 @@
-import React, {
-  Component
-} from 'react';
-import withStyles from 'material-ui/styles/withStyles';
-
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
 import AppGroup from './AppGroup';
 import appGroups from './assets/app-groups.json';
 
-const styles = theme => ({
+const styles = () => ({
   root: {
     overflow: 'hidden',
   },
 });
 
-class Apps extends Component {
-  createRows = appGroups => {
-    const {
-      classes
-    } = this.props;
-    let ret = [];
+class Apps extends React.Component {
+  createRows = appGroupsArr => {
+    const { classes } = this.props;
+    const ret = [];
     let i = 0;
-    while (i < appGroups.length) {
+    while (i < appGroupsArr.length) {
       ret.push(
         <div key={ret.length} className={classes.root}>
-          <div className='row'>
-            <div className='col-md-4'>
-              <AppGroup key={i} appGroup={appGroups[i]} />
+          <div className="row">
+            <div className="col-md-4">
+              <AppGroup key={i} appGroup={appGroupsArr[i]} />
             </div>
-            <div className='col-md-4'>
-              <AppGroup key={i+1} appGroup={appGroups[i+1]} />
+            <div className="col-md-4">
+              <AppGroup key={i + 1} appGroup={appGroupsArr[i + 1]} />
             </div>
-            <div className='col-md-4'>
-              <AppGroup key={i+2} appGroup={appGroups[i+2]} />
+            <div className="col-md-4">
+              <AppGroup key={i + 2} appGroup={appGroupsArr[i + 2]} />
             </div>
           </div>
-        </div>
+        </div>,
       );
       i += 3;
     }
     return ret;
-  }
+  };
   render() {
     return (
       <div>
-        {this.createRows(appGroups).map((row, i)=>{
+        {this.createRows(appGroups).map(row => {
           return row;
         })}
       </div>
     );
   }
 }
+
+Apps.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
 export default withStyles(styles)(Apps);
