00c756854a4be4759f9cb21145d15b317daa907f
Daniel Lennox <dlennox2424@gmail.com>
2019-05-23 10:10:37 -0600

    [5.0.1] Cleaning up code and fixing CodeClimate issues

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
+import AppFrame, { createConfig } from 'colostate-ricro-ui';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Route } from 'react-router-dom';
+import config from './assets/config';
+import Apps from './components/Apps';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routes = [{ path: '/', component: Apps }];
+
+ReactDOM.render(
+  <AppFrame config={createConfig(config)}>
+    {routes.map(route => (
+      <Route component={route.component} exact key={route.path} path={route.path} />
+    ))}
+  </AppFrame>,
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
